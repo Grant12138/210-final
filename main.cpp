dd1c9db60e35165fab803e992c985dfaaf60e5f5
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-12-09 19:02:53 -0800

    10-minute update

--- main.cpp
+++ main.cpp
@@ -15,14 +15,52 @@ class Queue
 {
     private:
         Node* head;
+        Node* tail;
 
     public:
-        Queue()
+        Queue() : head(nullptr), tail(nullptr) {}
+
+        ~Queue()
+        {
+            while (head != nullptr)
+            {
+                Node* temp = head;
+                head = head->next;
+                delete temp;
+            }
+        }
+
+        void enqueue(const string& name, const string& drink)
+        {
+            Node* newNode = new Node(name, drink);
+            if (tail == nullptr)
+                head = tail = newNode;
+            else
+            {
+                tail->next = newNode;
+                tail = newNode;
+            }
+        }
+
+        void displayQueue() const
+        {
+            cout << "Current Queue: ";
+            Node* current = head;
+            while (current != nullptr)
+            {
+                cout << "[" << current->name << " - " << current->drink << "]";
+                current = current->next;
+            }
+            cout << '\n';
+        };
 };
 
 int main()
 {
+    srand(time(0));
 
+    // Possible names and drinks
+    string names[] = {""};
 
     return 0;
 }
